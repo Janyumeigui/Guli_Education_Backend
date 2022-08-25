1cf54915aee86ce30ebbc5a1c8763eeaf628d89b
liyang <1840345547@qq.com>
2022-08-25 17:12:43 +0800

    订单管理模块

---
+++ service/service_order/src/main/java/com/zh/oes/order/mapper/PayOrderMapper.java
@@ -0,0 +1,16 @@
+package com.zh.oes.order.mapper;
+
+import com.zh.oes.model.entity.order.PayOrder;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 订单 Mapper 接口
+ * </p>
+*
+ */
+@Mapper
+public interface PayOrderMapper extends BaseMapper<PayOrder> {
+
+}
